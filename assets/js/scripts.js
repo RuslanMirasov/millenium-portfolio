c1c6ceef0ea4a2dddb14f25e26e220896a912005
Ruslan <info@mirasov.dev>
2025-04-05 20:49:17 +0200

    init

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,67 @@
-const monthNames = ['январе', 'феврале', 'марте', 'апреле', 'мае', 'июне', 'июле', 'августе', 'сентябре', 'октябре', 'ноябре', 'декабре'];
-const monthCurrent = monthNames[new Date().getMonth()];
 const burger = document.querySelector('[data-js="burger"]');
 const navigation = burger.closest('header').querySelector('.navigation');
-
-document.querySelector('#month').innerText = monthCurrent;
-
-export const debounce = (func, wait = 200) => {
-  let timeout;
-  return function (...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), wait);
-  };
-};
+const portfolioNavigation = document.querySelector('[data-js="portfolio-navigation"]');
+const portfolio = [
+  { category: 'hairdresser', items: [1, 2, 3, 4, 5] },
+  { category: 'nail', items: [1, 2, 3, 4, 5, 6, 7, 8] },
+  { category: 'brow', items: [] },
+  { category: 'mekup', items: [] },
+  { category: 'cosmetology', items: [] },
+];
 
 const toggleMenu = e => {
   burger.classList.toggle('open');
   navigation.classList.toggle('open');
 };
 
+const createPortfolioMarkup = ({ category, items }) => {
+  let categoryHTML = '';
+  const portfolioEl = document.querySelector('.portfolio__collection');
+  portfolioEl.classList.remove('active');
+  setTimeout(() => {
+    if (!items || !category || items.length === 0) {
+      categoryHTML = '<p>Категория пуста...</p>';
+    } else {
+      categoryHTML = items
+        .map(
+          item => `
+            <li>
+              <a href="./assets/img/portfolio/${category}/${item}.webp" data-fancybox="gallery" data-src="./assets/img/portfolio/${category}/${item}.webp">
+                <img src="./assets/img/portfolio/${category}/${item}.webp" width="200" height="150" alt="" />
+              </a>
+            </li>
+    `
+        )
+        .join('');
+    }
+
+    portfolioEl.innerHTML = categoryHTML;
+
+    portfolioEl.classList.add('active');
+  }, 300);
+};
+
+const changePrtfolioCategory = async e => {
+  if (!e.target.dataset.category) return;
+
+  const activeButton = document.querySelector('.portfolio__button.active');
+  const button = e.target;
+  const category = button.dataset.category;
+  const categoryCollection = portfolio.find(item => item.category === category);
+
+  if (activeButton) activeButton.classList.remove('active');
+  if (button) button.classList.add('active');
+
+  try {
+    button.classList.add('loading');
+    await createPortfolioMarkup(categoryCollection);
+  } finally {
+    setTimeout(() => {
+      button.classList.remove('loading');
+    }, 300);
+  }
+};
+
+createPortfolioMarkup(portfolio[0]);
 burger.addEventListener('click', toggleMenu);
+portfolioNavigation.addEventListener('click', changePrtfolioCategory);
